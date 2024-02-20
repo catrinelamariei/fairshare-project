b9713ab7e188052def441eb45323379b7a3f6aec
Nayden Borodjiev <n.t.borodjiev@student.tudelft.nl>
2024-02-21 00:21:17 +0100

    AddedFields to Transaction

--- commons/src/main/java/commons/Transaction.java
+++ commons/src/main/java/commons/Transaction.java
@@ -17,16 +17,13 @@ package commons;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
+import jakarta.persistence.*;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import java.sql.Date;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 public class Transaction {
@@ -34,16 +31,20 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public long id;
-
+    @Column(columnDefinition = "TIMESTAMP(0)")
     public Date date;
-
+    public String currencyCode;
+    @Column(columnDefinition = "NUMERIC(21,2)")
+    public BigDecimal amount;
     @SuppressWarnings("unused")
     private Transaction() {
         // for object mapper
     }
 
-    public Transaction(Date date) {
+    public Transaction(Date date,String currencyCode, BigDecimal amount) {
         this.date = date;
+        this.currencyCode = currencyCode;
+        this.amount = amount;
     }
 
     @Override
