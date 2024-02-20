6d74b931c13cbe26b98941a7ffe048c9c134a4ab
Runan Han <r.han-3@student.tudelft.nl>
2024-02-20 20:38:54 +0100

    created Event class with basic fields

---
+++ commons/src/main/java/commons/Event.java
@@ -0,0 +1,57 @@
+package commons;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    public String name;
+    public List<String> tags;
+
+    /**
+     * Empty constructor for event
+     */
+    @SuppressWarnings("unused")
+    private Event() {
+        // for object mapper
+    }
+
+    /**
+     * Constructure for event
+     * @param name  name of the event
+     * @param tags  list of tags the event is associated to
+     */
+    public Event(String name, List<String> tags) {
+        this.name = name;
+        this.tags = tags;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
+}
