bee82bd387264afa8edddad894b8058c17c7c438
Arda Tamgaç <A.Tamgac@student.tudelft.nl>
2024-02-21 14:40:20 +0100

    fixing the pipeline

--- commons/src/test/java/commons/ParticipantTest.java
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -1,6 +1,5 @@
 package commons;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
