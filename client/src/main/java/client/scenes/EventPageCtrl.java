a106de6fde6034a6811103d10155909dd8f94b86
Dockx Benjamin <bdockx@student.tudelft.nl>
2024-02-22 09:04:08 +0100

    window view for transactions - added some windows to subdivide overview into transactions, participants, statistics - this left some of the other text redundant

--- client/src/main/java/client/scenes/EventPageCtrl.java
+++ client/src/main/java/client/scenes/EventPageCtrl.java
@@ -10,16 +10,21 @@ import java.util.ResourceBundle;
 public class EventPageCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    private final Integer eventId;
 
     @Inject
     public EventPageCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
+        this.eventId = 1; //temporary placeholder
     }
 
     public void initialize(URL location, ResourceBundle resources) {}
 
-    public void nextPage() {
+    public void gotoHome() {
+        mainCtrl.showStartPage();
+    }
+    public void gotoAdminLogin() {
         mainCtrl.showAdminPage();
     }
 }
