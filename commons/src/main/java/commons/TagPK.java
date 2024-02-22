abf70d78eebb190561796528e015629acf1294f6
Runan Han <r.han-3@student.tudelft.nl>
2024-02-23 00:48:29 +0100

    Create helper class for Tag's composite key

---
+++ commons/src/main/java/commons/TagPK.java
@@ -0,0 +1,38 @@
+package commons;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+public class TagPK implements Serializable {
+
+    public long eventId;
+    public String name;
+
+    public TagPK() {}
+
+    public TagPK(long eventId, String name) {
+        this.eventId = eventId;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
+
+}
