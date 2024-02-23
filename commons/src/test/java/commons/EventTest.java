5abb5d09ece9985cacf4a72a10212023b3b49fba
Runan Han <r.han-3@student.tudelft.nl>
2024-02-23 01:13:13 +0100

    Updated test for events after adjusting to new Event class. Failing some tests because the assertEquals method fails for the two sets which have equal internal components for some reason. Hmmmmmm

--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -1,86 +1,77 @@
 package commons;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static commons.Tag.Color.BLUE;
+import static commons.Tag.Color.RED;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
 
     private Event e;
-    private List<String> tags;
+    private Tag t;
+    private Set<Tag> tags;
+
+    @BeforeEach
+    void setup() {
+        e = new Event("name");
+        t = new Tag(e, "name", BLUE);
+        tags = new HashSet<>();
+        tags.add(t);
+    }
 
 
     @Test
     void checkConstructor() {
-        tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
-        e = new Event("name", tags);
         assertEquals("name", e.name);
+        assertTrue(e.addTag(t));
         assertEquals(tags, e.tags);
     }
 
     @Test
     void equalsSame() {
-        tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
-        e = new Event("name", tags);
-        List<String> tags2 = new ArrayList<>();
-        tags2.add("tag1");
-        tags2.add("tag2");
-        Event e2 = new Event("name", tags2);
+        Event e2 = new Event("name");
+        Tag t2 = new Tag(e2, "name", BLUE);
+        Set<Tag> tags2 = new HashSet<>();
+        tags2.add(t2);
         assertEquals(e, e2);
     }
 
     @Test
     void equalsDifferentName() {
-        tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
-        e = new Event("name", tags);
-        List<String> tags2 = new ArrayList<>();
-        tags2.add("tag1");
-        tags2.add("tag2");
-        Event e2 = new Event("name2", tags2);
+        Event e2 = new Event("name2");
+        Tag t2 = new Tag(e2, "name", BLUE);
+        Set<Tag> tags2 = new HashSet<>();
+        tags2.add(t2);
         assertNotEquals(e, e2);
     }
 
     @Test
     void equalsDifferentTags() {
-        tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
-        e = new Event("name", tags);
-        List<String> tags2 = new ArrayList<>();
-        tags2.add("tag1");
-        tags2.add("tag3");
-        Event e2 = new Event("name", tags2);
+        Event e2 = new Event("name");
+        Tag t2 = new Tag(e2, "name2", RED);
+        Set<Tag> tags2 = new HashSet<>();
+        tags2.add(t2);
         assertNotEquals(e, e2);
     }
 
     @Test
     void equalsHashCode() {
-        tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
-        e = new Event("name", tags);
-        List<String> tags2 = new ArrayList<>();
-        tags2.add("tag1");
-        tags2.add("tag2");
-        Event e2 = new Event("name", tags2);
+        Event e2 = new Event("name");
+        Tag t2 = new Tag(e2, "name", BLUE);
+        Set<Tag> tags2 = new HashSet<>();
+        tags2.add(t2);
         assertEquals(e.hashCode(), e2.hashCode());
     }
 
     @Test
     void testToString() {
-        tags = new ArrayList<>();
-        tags.add("tag1");
-        tags.add("tag2");
-        e = new Event("name", tags);
+        e = new Event("name");
         assertNotNull(e.toString());
     }
 }
