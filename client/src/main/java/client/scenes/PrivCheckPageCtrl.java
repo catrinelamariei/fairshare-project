24ca5cfab15a1554805369b6f573e214a41f6d36
J90-arch <jokubasvirsilas@gmail.com>
2024-02-23 11:26:17 +0100

    added a static password check for the admin login

--- client/src/main/java/client/scenes/PrivCheckPageCtrl.java
+++ client/src/main/java/client/scenes/PrivCheckPageCtrl.java
@@ -2,6 +2,8 @@ package client.scenes;
 
 import client.MainCtrl;
 import client.utils.ServerUtils;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import java.net.URL;
@@ -10,16 +12,24 @@ import java.util.ResourceBundle;
 public class PrivCheckPageCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    public TextField password;
+    public Text text;
 
     @Inject
     public PrivCheckPageCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
+        text = new Text();
     }
 
     public void initialize(URL location, ResourceBundle resources) {}
 
     public void adminPage() {
-        mainCtrl.showAdminPage();
+        if(password.getText().equals("abc")) {
+            mainCtrl.showAdminPage();
+        }else{
+            text.setStyle("-fx-text-fill: red;");
+            text.setText("Wrong code");
+        }
     }
 }
