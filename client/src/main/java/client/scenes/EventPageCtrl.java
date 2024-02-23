b8702847659a21978672b28f54349987d887d52a
Dockx Benjamin <bdockx@student.tudelft.nl>
2024-02-23 13:42:29 +0100

    Removed duplicate text and added functionality to invitebutton - invitebutton now copies a string to the clipoard although it's contents are still placeholders

--- client/src/main/java/client/scenes/EventPageCtrl.java
+++ client/src/main/java/client/scenes/EventPageCtrl.java
@@ -4,6 +4,9 @@ import client.MainCtrl;
 import client.utils.ServerUtils;
 
 import javax.inject.Inject;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,5 +29,22 @@ public class EventPageCtrl {
     }
     public void gotoAdminLogin() {
         mainCtrl.showAdminPage();
+    }
+
+    public void copyInviteCode() {
+        //get system singleton of clipboard
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+        //get invite code (String)
+        String invCode = "123456789"; //temporary placeholder (content)
+        String eventName = "NewYearEvent"; //temporary placeholder (owner)
+
+        //copy data to clipboard
+        StringSelection content = new StringSelection(invCode);
+        StringSelection owner = new StringSelection(eventName);
+        clipboard.setContents(content, owner);
+
+        //display copied for 3 seconds
+        System.out.println("Copied to clipboard!"); //temporary placeholder
     }
 }
