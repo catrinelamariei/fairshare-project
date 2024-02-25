b42ff89f8aaac266ba977db7ef5ca2652064b51f
Runan Han <r.han-3@student.tudelft.nl>
2024-02-25 13:10:40 +0100

    fixed pipeline error

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -3,8 +3,6 @@ package commons;
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 import jakarta.persistence.*;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.util.HashSet;
