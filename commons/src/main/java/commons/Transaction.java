204063a36e6fe19f2d92424b25d51f8e4954e3ff
Nayden Borodjiev <n.t.borodjiev@student.tudelft.nl>
2024-03-26 23:32:38 +0100

    fix checkstyle

--- commons/src/main/java/commons/Transaction.java
+++ commons/src/main/java/commons/Transaction.java
@@ -2,8 +2,6 @@ package commons;
 
 import commons.DTOs.TransactionDTO;
 import jakarta.persistence.*;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.math.BigDecimal;
