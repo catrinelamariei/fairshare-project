0023b04e76ebe6cc6a1876a48419b867f6a1eb47
Benjamin Dockx <b.h.p.dockx@student.tudelft.nl>
2024-03-27 16:19:01 +0100

    fixed pipeline

--- client/src/main/java/client/UserData.java
+++ client/src/main/java/client/UserData.java
@@ -3,12 +3,8 @@ package client;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javafx.scene.layout.VBox;
 
 import java.io.File;
 import java.io.IOException;
