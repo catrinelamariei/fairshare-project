44228757be41e2bfeadce35d78b5de50c820b395
Jokūbas Viršilas <J.Virsilas@student.tudelft.nl>
2024-04-01 01:51:03 +0200

    limit of 2 requests per second

--- server/src/main/java/server/Authentication/PrivCheckController.java
+++ server/src/main/java/server/Authentication/PrivCheckController.java
@@ -1,22 +1,34 @@
 package server.Authentication;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import server.Authentication.CodeGenerator;
+import server.Authentication.JwtTokenService;
+import server.Authentication.TokenBucket;
+
 @RestController
 public class PrivCheckController {
 
     private static String code; // need to generate random
     private JwtTokenService jwtTokenService;
+    private final TokenBucket tokenBucket = new TokenBucket(2, 1000);
 
     public PrivCheckController(){
         super();
         this.generatePassword();
     }
 
+    @Autowired
+    private HttpServletRequest request;
 
     @PostMapping("/admin")
     @ResponseBody
-    public String checkPassword(@RequestBody String password, HttpServletRequest request) {
+    public String checkPassword(@RequestBody String password) {
+        if (!tokenBucket.tryConsume()) {
+            return "Too many requests. Please try again later.";
+        }
+
         if (password.equals(code)) {
             // Instantiate JwtTokenService
             JwtTokenService jwtTokenService = new JwtTokenService();
@@ -29,10 +41,13 @@ public class PrivCheckController {
         }
     }
 
-
     @GetMapping("/admin")
     @ResponseBody
     public String generatePassword(){
+        if (!tokenBucket.tryConsume()) {
+            return "Too many requests. Please try again later.";
+        }
+
         code = CodeGenerator.generateRandomString(6);
         System.out.println("Code: " + code);
         return "Code generated";
