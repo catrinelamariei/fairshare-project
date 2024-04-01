bd7811aa9a7a43a5813ed1d3a5a2638b6563c945
Jokūbas Viršilas <J.Virsilas@student.tudelft.nl>
2024-04-01 03:39:41 +0200

    working on it

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -6,14 +6,16 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import static com.google.inject.Guice.createInjector;
-
 
 public class Main extends Application {
 
     private static final Injector INJECTOR = createInjector(new MyModule());
     private static final MyFXML FXML = new MyFXML(INJECTOR);
+    private static final ResourceBundle messages = ResourceBundle.getBundle("client.Languages.EN");
 
     public static void main(String[] args) {
         launch();
@@ -21,17 +23,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        var startPage = FXML.load(StartPageCtrl.class, "client", "scenes", "StartPage.fxml");
-        var mainPage = FXML.load(StartPageCtrl.class, "client", "scenes", "StartPage.fxml");
-        var eventPage = FXML.load(EventPageCtrl.class, "client", "scenes", "EventPage.fxml");
-        var adminPage = FXML.load(AdminPageCtrl.class, "client", "scenes", "AdminPage.fxml");
-        var privCheckPage = FXML.load(PrivCheckPageCtrl.class, "client",
+        Locale locale = Locale.getDefault(); // Get default locale
+        var startPage = FXML.load(StartPageCtrl.class, messages, locale,
+                "client", "scenes", "StartPage.fxml");
+        var mainPage = FXML.load(StartPageCtrl.class, messages, locale,
+                "client", "scenes", "StartPage.fxml");
+        var eventPage = FXML.load(EventPageCtrl.class, messages, locale,
+                "client", "scenes", "EventPage.fxml");
+        var adminPage = FXML.load(AdminPageCtrl.class, messages, locale,
+                "client", "scenes", "AdminPage.fxml");
+        var privCheckPage = FXML.load(PrivCheckPageCtrl.class, messages, locale,
+                "client",
                 "scenes", "PrivCheckPage.fxml");
-        var transactionPage = FXML.load(TransactionPageCtrl.class, "client",
-            "scenes", "TransactionPage.fxml");
+        var transactionPage = FXML.load(TransactionPageCtrl.class, messages, locale,
+                "client",
+                "scenes", "TransactionPage.fxml");
 
         var mainCtrl = INJECTOR.getInstance(MainCtrl.class);
         mainCtrl.initialize(primaryStage, mainPage, eventPage, adminPage,
-            privCheckPage, startPage, transactionPage);
+                privCheckPage, startPage, transactionPage);
     }
 }
