3a217de5ba722c6846a1eace39ba324b2c531dc6
Nayden Borodjiev <n.t.borodjiev@student.tudelft.nl>
2024-04-03 19:22:49 +0200

    Testing controller

--- commons/src/main/java/commons/Currency/RateDTO.java
+++ commons/src/main/java/commons/Currency/RateDTO.java
@@ -2,6 +2,7 @@ package commons.Currency;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class RateDTO {
     public String currencyFrom;
@@ -18,5 +19,20 @@ public class RateDTO {
     }
 
     public RateDTO() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RateDTO rateDTO)) return false;
+        return Objects.equals(currencyFrom, rateDTO.currencyFrom) &&
+                Objects.equals(currencyTo, rateDTO.currencyTo) &&
+                Objects.equals(rate, rateDTO.rate) &&
+                Objects.equals(date, rateDTO.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currencyFrom, currencyTo, rate, date);
     }
 }
