6280e3dacd5a4a0bfd17854d079343cdb154e730
Nayden Borodjiev <n.t.borodjiev@student.tudelft.nl>
2024-04-04 17:57:27 +0200

    fixed un-imported package

--- server/src/test/java/server/api/ParticipantControllerTest.java
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -14,6 +14,7 @@ import server.database.ParticipantRepository;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
